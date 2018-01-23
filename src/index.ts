ecea519fa2b26b0bcef663527d8276c6a59ff7f1
PC-ALBERTO\Alberto <job.aelesfer@gmail.com>
2018-01-23 17:19:00 +0100

    Añadida peticion constante para evitar Slepp de heroku

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import Webapp from './components/webapp';
+import axios from 'axios';
 
-
-
-
+// Inicio de la aplicación web
 const port = process.env.PORT || 3000;
 const webapp = new Webapp();
 webapp.start().listen(port);
+
+// Programamos una peticion constante a Heroku para que no tire la aplicación
+setInterval(() => {
+    axios.get('http://roleando-online-bot.herokuapp.com');
+  }, 900000);
